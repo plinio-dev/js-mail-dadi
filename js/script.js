6e2530b597552a9be3aca289b7475d9ce49273fc
Tommaso P <tommaso.plinio@gmail.com>
2021-03-01 16:18:12 +0100

    Array emails

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ stampa un messaggio appropriato sull’esito del controllo.*/
 
 //1. chiedi all'utente la sua email
 
+//creo un array di email
+var listaEmail = [ "mario@gmail.com", "pippo@gmail.com", "chiara@hotmail.com" "ciro@gmail.com"];
+
+var emailUtente
+
+
 //2 controlla nella lista chi può accedere
 
 //3 stampa un messaggio appropriato sull’esito del controllo
