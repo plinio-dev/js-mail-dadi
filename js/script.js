3f10f6112442b8d19d317c7ca028ea1a19083de3
Tommaso P <tommaso.plinio@gmail.com>
2021-03-01 16:24:14 +0100

    Richiesta email

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.*/
 
+
 //1. chiedi all'utente la sua email
 
 //creo un array di email
-var listaEmail = [ "mario@gmail.com", "pippo@gmail.com", "chiara@hotmail.com" "ciro@gmail.com"];
+var listaEmail = [ "mario@gmail.com", "pippo@gmail.com", "chiara@hotmail.com", "ciro@gmail.com"];
 
-var emailUtente
+var emailUtente = prompt( "Inserisci la tua email" );
 
 
 //2 controlla nella lista chi può accedere
