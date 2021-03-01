1f471f0650a7a6125a0af196a5a293db6e7d11c6
Tommaso P <tommaso.plinio@gmail.com>
2021-03-01 16:14:49 +0100

    Start

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.*/
+
+//1. chiedi all'utente la sua email
+
+//2 controlla nella lista chi può accedere
+
+//3 stampa un messaggio appropriato sull’esito del controllo
